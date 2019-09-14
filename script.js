b497ac804e2d9bce5fdd57aec2130fac790ec6b0
Alexander Hansson <48253253+ahnm1@users.noreply.github.com>
2019-09-14 17:02:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+	const burgerMenu = document.querySelector('.burgerMenu');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burgerMenu.addEventListener('click', () => {
+
+// Toggle Nav
+		nav.classList.toggle('nav-active');
+
+// Animate Links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+				console.log(index / 3);
+			}
+			
+		});
+		// Burger Animation
+		burgerMenu.classList.toggle('toggle'); 
+	});
+
+}
+
+
+navSlide();

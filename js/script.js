1b632ec052c27bda59379562b016e44421f49203
Alexander Hansson <48253253+ahnm1@users.noreply.github.com>
2019-09-15 20:57:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,20 +9,21 @@ const navSlide = () => {
 		nav.classList.toggle('nav-active');
 
 // Animate Links
-		navLinks.forEach((link, index) => {
+
+        navLinks.forEach((link, index) => {
 			if (link.style.animation) {
 				link.style.animation = '';
+
 			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+				link.style.animation = `navLinkFade 0.3s ease forwards ${index / 5 + 0.3}s`;
 				console.log(index / 3);
-			}
-			
+
+			}	
 		});
+
 		// Burger Animation
 		burgerMenu.classList.toggle('toggle'); 
-	});
-
+	});	
 }
-
 
 navSlide();
